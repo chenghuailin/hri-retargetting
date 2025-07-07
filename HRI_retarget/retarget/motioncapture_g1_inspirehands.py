Correct rotation matrix, enable frame-wise global config computation,

HRI_retarget/retarget/motioncapture_g1_inspirehands.py
@@ -30,7 +30,9 @@ import yaml
 import pandas as pd
 
 # rot = np.eye(3)
-rot = np.array([[0,-1,0],[1,0,0],[0,0,1]])
+rot = np.array([[0,0,1],
+                [1,0,0],
+                [0,1,0]])
 left_hand_to_inspire = np.array([[1,0,0],[0,0,1],[0,-1,0]])
 right_hand_to_inspire = np.array([[-1,0,0],[0,0,1],[0,1,0]])
 config_file_path = os.path.join(DATA_ROOT, "resources/robots/g1_inspirehands/inspire_hand.yml")
@@ -43,17 +45,20 @@ if __name__ == "__main__":
         quit()
 
     filename = sys.argv[1]
-    bvh_joint_local_coord, _ = Get_bvh_joint_pos_and_Rot(filename, link_list = MOTION_CAPTURE_LINKS)
-    # bvh_joint_global_coord = Get_bvh_joint_global_pos(filename, link_list=MOTION_CAPTURE_LINKS)
+    # bvh_joint_local_coord_pos, bvh_joint_local_coord_rot = Get_bvh_joint_pos_and_Rot(filename, link_list = MOTION_CAPTURE_LINKS)
+    # bvh_joint_local_coord_pos, _ = Get_bvh_joint_pos_and_Rot(filename, link_list = MOTION_CAPTURE_LINKS)
+    bvh_joint_global_coord_pos = Get_bvh_joint_global_pos(filename, link_list = MOTION_CAPTURE_LINKS)
 
 
-    num_frames = len(bvh_joint_local_coord)
+    # num_frames = len(bvh_joint_local_coord_pos)
+    num_frames = len(bvh_joint_global_coord_pos)
     print("Num of frames: ", num_frames)
     
     model = G1_Inspirehands_Motion_Model(batch_size=num_frames, joint_correspondence=MOTION_CAPTURE_G1_INSPIREHANDS_CORRESPONDENCE)
 
     rot_batch = torch.from_numpy(rot).view(1,3,3).repeat(num_frames,1,1).type(torch.float)
-    model.set_gt_joint_positions(torch.bmm(bvh_joint_local_coord,rot_batch.transpose(1,2)))
+    # model.set_gt_joint_positions(torch.bmm(bvh_joint_local_coord_pos, rot_batch.transpose(1,2)))
+    model.set_gt_joint_positions(torch.bmm(bvh_joint_global_coord_pos,rot_batch.transpose(1,2)))
     # model.set_gt_joint_positions(bvh_joint_local_coord @ rot.T)
     print("Links of robot: ", model.chain.get_link_names())
     print(model.global_trans)
@@ -66,12 +71,12 @@ if __name__ == "__main__":
     # best_loss = float('inf')
     # best_model_state = None
     
-    pbar = tqdm(range(2000))
+    pbar = tqdm(range(200))
     for epoch in pbar:
         
         ### normalize
         with torch.no_grad():
-            # model.refine_wrist_angle()
+            # model.refine_wrist_angle()reshape(3, 1)
             model.normalize()
     
         joint_local_velocity_loss, joint_local_accel_loss = model.joint_local_velocity_loss()
@@ -85,9 +90,9 @@ if __name__ == "__main__":
         # elbow_loss = model.elbow_loss()
         
         loss_dict = {
-            "joint_global_position_loss": [2.0, joint_global_position_loss],
+            "joint_global_position_loss": [2.5, joint_global_position_loss],
             "joint_local_velocity_loss": [1.0, joint_local_velocity_loss],
-            "joint_local_accel_loss": [0.0, joint_local_accel_loss],
+            "joint_local_accel_loss": [0.5, joint_local_accel_loss],
             "dof_limit_loss": [1.0, dof_limit_loss],
             # "orientation_loss": [0.3, orientation_loss],
             "collision_loss": [1.0, collision_loss],